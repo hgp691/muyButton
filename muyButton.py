75de122fd77be69894ff809a1df265fb8c66d585
Horacio Guzman <horacioguzman@MacBook-Pro-de-Horacio.local>
2018-11-21 15:26:30 -0500

    hero

--- muyButton.py
+++ muyButton.py
@@ -1,9 +1,17 @@
 import RPi.GPIO as GPIO
 import time
-import request
+import requests
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.IN)
 GPIO.setup(4, GPIO.OUT)
 
-print "OK"
+
+
+while true:
+	btnInput = GPIO.input(24)
+	if btnInput == True:
+		print("Pressed")
+	else:
+		print("Released")
+	time.sleep(1)
